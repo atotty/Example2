6b7bdbeb281a00b7ae78ca1121495b3038bd9766
windward_faculty <atotty@windwardschool.org>
2018-05-21 08:27:02 -0700

    Change test_branch comments

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world"); 
+        System.out.println("hello world");
+
+        // This is a comment that I added in the branch test_branch.
+        // Let's see what happens to this stuff... 
     }
 }
